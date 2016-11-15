81c5add533b6f57abd4c503f70f1618488d62dd9
Yip <yipjiajie@gmail.com>
2016-11-15 18:56:55 +0800

    change

--- msi_cache.py
+++ msi_cache.py
@@ -2,29 +2,36 @@ from cache import Cache
 from msi_cache_block import CacheBlock
 
 class CacheSet():
-    def __init__(self, cache, n_blocks, i):
-        self.cache_blocks = CacheBlock(cache.id, i)
+    def __init__(self, cache, assoc, i):
+        self.cache_blocks =  [
+            CacheBlock(assoc, i)
+            for i in range(assoc)
+        ]
 
     def get_block_for_tag(self, idx):
-        return self.cache_blocks
+        return self.cbr()
 
     def get_summary(self):
-        return self.cache_blocks.get_summary()
+        return self.cbr().get_summary()
 
     def processor_action(self, event):
-        return self.cache_blocks.processor_action(event)
+        return self.cbr().processor_action(event)
 
     def bus_action(self, event, origin):
-        return self.cache_blocks.bus_action(event, origin)
+        return self.cbr().bus_action(event, origin)
 
     def commit(self, ic):
-        return self.cache_blocks.commit(ic)
+        return self.cbr().commit(ic)
+
+    def cbr(self):
+        # Choose correct block
+        return self.cache_blocks[0]
 
 
 class MsiCache(Cache):
     def init_cache_sets(self, cache_id):
         return [
 
-            CacheSet(self, self.n_blocks, i)
+            CacheSet(self, self.assoc, i)
             for i in range(self.n_blocks)
         ]
